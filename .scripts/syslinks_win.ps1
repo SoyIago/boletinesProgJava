8a14ead8c06c77522955ba00ba8dd9f8762cbe8d
Iago Xoan Casal Neira <soyiagox009@gmail.com>
2022-12-09 11:12:30 +0100

    Añadido script para generar los syslinks del proyecto en Windows

---
+++ .scripts/syslinks_win.ps1
@@ -0,0 +1,19 @@
+if (!$(Get-CimInstance -ClassName CIM_OperatingSystem).Caption.Contains("Windows")) {
+	exit
+}
+if ($pwd -ccontains "/.scripts") {
+	exit
+}
+cd ..
+$packages = Get-ChildItem -Name -Directory | Select-String -pattern '[0-9]{1,2}_B*'
+cd .netbeans\src
+$currentSysLinks = Get-ChildItem -Name -Directory | Select-String -pattern "B[0-9]{1,2}_*"
+for ($i = 0; $i -lt $currentSysLinks.length; $i++) {
+	Remove-Item $currentSysLinks[$i] -Force -Confirm:$false
+}
+
+for ($i = 0; $i -lt $packages.length; $i++) {
+	$pkgName = 'B' + $($i + 1) + $packages[$i] -replace "[0-9]_Boletin", "_"
+	$sourceDir = "..\..\" + $packages[$i]
+	cmd /c mklink /J $pkgName $sourceDir
+}
