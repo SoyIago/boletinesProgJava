4579f3f0e683d19f841404a73199090071a15250
Iago Xoan Casal Neira <soyiagox009@gmail.com>
2022-12-19 10:51:31 +0100

    Script renombrado y arreglado.

--- .scripts/symlinks_win.ps1
+++ .scripts/symlinks_win.ps1
@@ -7,13 +7,13 @@ if ($pwd -ccontains "/.scripts") {
 cd ..
 $packages = Get-ChildItem -Name -Directory | Select-String -pattern '[0-9]{1,2}_B*'
 cd .netbeans\src
-$currentSysLinks = Get-ChildItem -Name -Directory | Select-String -pattern "B[0-9]{1,2}_*"
+$currentSysLinks = Get-ChildItem -Name | Select-String -pattern "B[0-9]{1,2}_*"
 for ($i = 0; $i -lt $currentSysLinks.length; $i++) {
-	Remove-Item $currentSysLinks[$i] -Force -Confirm:$false
+	Remove-Item $currentSysLinks[$i] -Force
 }
 
 for ($i = 0; $i -lt $packages.length; $i++) {
-	$pkgName = 'B' + $($i + 1) + $packages[$i] -replace "[0-9]_Boletin", "_"
+	$pkgName = 'B' + $($i) + $packages[$i] -replace "[0-9]_Boletin", "_"
 	$sourceDir = "..\..\" + $packages[$i] + "\src"
 	cmd /c mklink /J $pkgName $sourceDir
 }
